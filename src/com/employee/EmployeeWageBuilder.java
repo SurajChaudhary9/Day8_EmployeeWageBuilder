79c0fb30187b6245d4b0d1432f08ac7118817f92
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-13 19:36:35 +0530

    Added : Check for Employee wage full and hal time accordingly

--- src/com/employee/EmployeeWageBuilder.java
+++ src/com/employee/EmployeeWageBuilder.java
@@ -1,14 +1,31 @@
 package com.employee;
 
-public class EmployeeWageBuilder {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Program");
+import java.util.Random;
 
-		int IS_FULL_TIME = 1;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+public class EmployeeWageBuilder {
+	static final int PRESENT = 1;
+	static final int WAGE_PER_HOUR = 20;
+	static final int FULL_DAY_HOUR = 8;
+
+	public static int empCheck() {
+		Random random = new Random();
+		int empCheck = random.nextInt(2);
+		return empCheck;
+	}
+
+	public static void employeeWage() {
+		int empWage = 0;
+		int empcheck = empCheck();
+		if (empcheck == PRESENT) {
+			empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+		}
+		System.out.println("The wage of employee is :  = " + empWage);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee wage Builder");
+
+		employeeWage();
+
 	}
 }
